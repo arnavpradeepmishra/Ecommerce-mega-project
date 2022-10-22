553ab4659b4e017d846b592791d14ef6677b0d74
arnavpradeepmishra <iranjanamishra@gmail.com>
2022-10-22 15:53:43 +0530

    first commit

---
+++ action_page.php
@@ -0,0 +1,8 @@
+<body>
+  <h1> thank you for your purchase your items will be dilverd to you  
+any feed back
+  <label for="Name">Name:</label>
+<input type="text" id="Name" name="Name" placeholder="Jane Doe">
+<label for="feedback">feedback:</label>
+<input type="text" id="feedback" name="feedback"placeholder="any thing">
+</body>
